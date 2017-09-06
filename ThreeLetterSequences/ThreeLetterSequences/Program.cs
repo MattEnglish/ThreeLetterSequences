94c52bf8c8288c1fb9f11706a87a0c548cdfa486
MattEnglish <31655162+MattEnglish@users.noreply.github.com>
2017-09-06 11:07:42 +0100

    Added Regex tra counter

--- ThreeLetterSequences/ThreeLetterSequences/Program.cs
+++ ThreeLetterSequences/ThreeLetterSequences/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ThreeLetterSequences
 {
@@ -10,6 +12,22 @@ namespace ThreeLetterSequences
     {
         static void Main(string[] args)
         {
+            String s = System.IO.File.ReadAllText("SampleText.txt");
+            int traCount = traCounter(s);
+            Console.WriteLine(traCount);
+
+            
         }
+
+        static int traCounter(string s)
+        {
+            int counter = 0;
+            Regex rx = new Regex("(tra)");
+            MatchCollection matches = rx.Matches(s);
+            return matches.Count;
+            
+        }
+        
+
     }
 }
